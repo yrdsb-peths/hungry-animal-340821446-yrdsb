b0d077c1925c6e1003b7206538d41dd74a0dd0db
Martin Baldwin <340821446@gapps.yrdsb.ca>
2022-05-02 10:18:00 -0400

    Small clean up

--- Mouse.java
+++ Mouse.java
@@ -1,25 +1,17 @@
 import greenfoot.*;
 
 public class Mouse extends Actor {
-    private int velX = 2;
-    private int velY = 1;
-    
-    public Mouse() {
-        super();
-    }
-    
-    /**
-     * Act - do whatever the Mouse wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    public void act() {
-        int newX = getX() + velX;
-        int newY = getY() + velY;
-        setLocation(newX, newY);
-        World world = getWorld();
-        if (newX <= 0 || newX >= world.getWidth() - 1)
-            velX = -velX;
-        if (newY <= 0 || newY >= world.getHeight() - 1)
-            velY = -velY;
-    }
+	private int velX = 2;
+	private int velY = 1;
+
+	public void act() {
+		int newX = getX() + velX;
+		int newY = getY() + velY;
+		setLocation(newX, newY);
+		World world = getWorld();
+		if (newX <= 0 || newX >= world.getWidth() - 1)
+			velX = -velX;
+		if (newY <= 0 || newY >= world.getHeight() - 1)
+			velY = -velY;
+	}
 }
--- MyWorld.java
+++ MyWorld.java
@@ -1,10 +1,10 @@
 import greenfoot.*;
 
 public class MyWorld extends World {
-    public MyWorld() {
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1);
-        Mouse mouse = new Mouse();
-        addObject(mouse, 300, 200);
-    }
+	public MyWorld() {
+		// Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+		super(600, 400, 1);
+		Mouse mouse = new Mouse();
+		addObject(mouse, 300, 200);
+	}
 }
