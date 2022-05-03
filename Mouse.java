369558286071762e066222cd1bedc1140d6b823c
Martin Baldwin <340821446@gapps.yrdsb.ca>
2022-05-03 10:21:21 -0400

    Add Mouse turbo mode (Space bar)

--- Mouse.java
+++ Mouse.java
@@ -5,14 +5,23 @@ public class Mouse extends Actor {
 	private int velY = 1;
 
 	public void act() {
-		int newX = getX() + velX;
-		int newY = getY() + velY;
+		// Get change in position, 4x if Space bar held
+		int dx = velX;
+		int dy = velY;
+		if (Greenfoot.isKeyDown("space")) {
+			dx *= 4;
+			dy *= 4;
+		}
+		int newX = getX() + dx;
+		int newY = getY() + dy;
 		setLocation(newX, newY);
+		// Bounce off of the edges of the world
 		World world = getWorld();
 		if (newX <= 0 || newX >= world.getWidth() - 1)
 			velX = -velX;
 		if (newY <= 0 || newY >= world.getHeight() - 1)
 			velY = -velY;
+		// Eat Dots
 		removeTouching(Dot.class);
 	}
 }
