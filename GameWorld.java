831358d2669e5fefbb7ae9e985ace10986ddf3fb
Martin Baldwin <340821446@gapps.yrdsb.ca>
2022-05-25 10:13:22 -0400

    Add mouse

--- GameWorld.java
+++ GameWorld.java
@@ -9,6 +9,7 @@ public class GameWorld extends World {
 	public GameWorld() {
 		// Create a new world with 600x400 cells with a cell size of 1x1 pixels.
 		super(600, 400, 1);
+		addObject(new Mouse(), 100, 100);
 		addObject(new Canvas(600, 400), 300, 200);
 
 		timer = new SimpleTimer();
---
+++ Mouse.java
@@ -0,0 +1,34 @@
+import greenfoot.*;
+
+public class Mouse extends Actor {
+	private SimpleTimer timer;
+	private double x;
+	private double y;
+	private double speedX;
+	private double speedY;
+
+	public Mouse() {
+		timer = new SimpleTimer();
+	}
+
+	protected void addedToWorld(World world) {
+		x = (double) getX();
+		y = (double) getY();
+		reorient();
+	}
+
+	private void reorient() {
+		speedX = (double) (Greenfoot.getRandomNumber(40) - 20) / 10.0;
+		speedY = (double) (Greenfoot.getRandomNumber(40) - 20) / 10.0;
+		timer.mark();
+	}
+
+	public void act() {
+		if (timer.millisElapsed() > 3000 || isAtEdge()) {
+			reorient();
+		}
+		x += speedX;
+		y += speedY;
+		setLocation((int) x, (int) y);
+	}
+}
